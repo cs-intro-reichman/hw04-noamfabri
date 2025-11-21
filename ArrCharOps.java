ede7ebaa2540d033cfa4f8eadf6bd201457af3ba
noamfabri87 <noamfabri87@gmail.com>
2025-11-21 17:03:49 +0200

    Arr mid proccess

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -93,7 +93,7 @@ public class ArrCharOps {
        if (arr.length <= 0){
             return -1;
         }
-            for (int i = arr.length - 1 ; i > 0; i--){
+            for (int i = arr.length - 1 ; i >= 0; i--){
                 if (arr[i] == ch) {
                 return i;
             }
